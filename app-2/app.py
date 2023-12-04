54162c4900e0645d41a88fbbb516ac42035f3f78
fazledyn <rabidahamed@gmail.com>
2023-12-04 23:07:18 +0600

    Updated app-2 example

--- app-2/app.py
+++ app-2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,14 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route("/file/")
+def file():
+    file = request.args.get("name")
+    try:
+        content = open(file, "r").read()
+    except:
+        content = "The file couldn't be found. Are you sure you put correct file name?"
+    return content
 
 if __name__ == "__main__":
     app.run(debug=True)
